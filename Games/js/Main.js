952744657e495764b1f68004cbc57b76bc738ead
Sang Liang <samuellovefree@live.com>
2014-07-17 20:54:44 +0800

    Signed-off-by: Sang Liang <samuellovefree@live.com>

--- Games/js/Main.js
+++ Games/js/Main.js
@@ -13,7 +13,8 @@ var imgData=[
 	{name:"meixi",path:"img/meixi.png"},
 	{name:'Argentina',path:'img/Argentina.png'},
     {name:'Brazil',path:'img/Brazil.png'},
-    {name:'football',path:'img/football.png'}
+    {name:'football',path:'img/football.png'},
+	{name:"whiteBtn2",path:"img/whiteBtn2.png"}
 ];
 var showFlag=[];
 var sound;
@@ -27,16 +28,13 @@ var resultScore;
 
 //---------主函数入口---------
 function main(){
+ LGlobal.webAudio = false;
+sound = new LSound();
 LGlobal.setDebug(true);
 backGroundLayer=new LSprite()	;
 backGroundLayer.graphics.drawRect(1,"#000",[0,0,900,600],true,"#000");
 addChild(backGroundLayer);
-/*
-Bound = new LSprite();
-backGroundLayer.addChild(Bound);
 
-Bound.graphics.drawRect(1,'#f00',[20,20,800,10],true,'#f00');
-*/
 loadingLayer=new LoadingSample1();
 backGroundLayer.addChild(loadingLayer);
     Bound();
@@ -82,19 +80,24 @@ function gameInit(result){
 
 //-----游戏场景-----
 function changePage(){
-/*	var back;
-    back = new LSprite();*/
 	LGlobal.box2d=new LBox2d();
 	backGroundLayer.die();
 	backGroundLayer.removeAllChild();
-  /*  back.graphics.drawRect(1,'#f00',[0,0,900,600],false);*/
 	var backGroundPic=new LBitmap(new LBitmapData(imglist["backGround"]));
 	backGroundLayer.addChild(backGroundPic);
-  /*  backGroundLayer.addChild(back);*/
-	onup();
+ 
+    //导入按钮图片
+    musicBtn = new LSprite();
+    musicBtn.x = 850;
+    musicBtn.y = 10;
+    backGroundLayer.addChild(musicBtn);
+    var bitmap = new LBitmapData(imglist['whiteBtn2']);
+    musicBtn.graphics.beginBitmapFill(bitmap);
+    musicBtn.graphics.drawRect(1,"#000",[0,0,41,29],false);
+    addChild(musicBtn);
+    musicBtn.addEventListener(LMouseEvent.MOUSE_UP,onup);
 	Bound();
 
-	
 	//-----------玩家1出现（梅西）------------
 	var firstPlayerLayer=new LSprite();
 	firstPlayerLayer.x=100;
@@ -116,8 +119,6 @@ function changePage(){
 	secondPlayerLayer.addBodyCircle(40,40,40,1,5,0.5,0.9);
 	secondPlayerLayer.setBodyMouseJoint(true);
 	
-	
-	
 	//-----足球-----
 	var ballLayer = new LSprite();
 	ballLayer.x=450;
@@ -127,8 +128,6 @@ function changePage(){
 	ballLayer.graphics.beginBitmapFill(bitmap);
 	ballLayer.graphics.drawArc(1,"#000",[20,20,20,0,2*Math.PI],false);
 	ballLayer.addBodyCircle(20,20,20,1,5,0.2,0.9);
-	
-	
 	
 	showFlag.push(new LBitmapData(imglist["Argentina"]));
 	showFlag.push(new LBitmapData(imglist["Brazil"]));
@@ -146,12 +145,8 @@ function changePage(){
      	backGroundLayer.addChild(Brazil);
 		
 		scoreText();
-	
 
-	
 }
-
-
 
 	//---国家比分-----
 	function scoreText(){
@@ -177,41 +172,48 @@ function changePage(){
      	resultBrazil.y = 10;
      	resultScore.addChild(resultBrazil);
 	}
-	//播放音乐
+	
+//播放音乐
 function onup(e){
-	sound=new LSound();
-	sound.load('sound/2014Soccer.mp3');
-	sound.addEventListener(LEvent.COMPLETE,loadOver);
+   if(sound.length == 0){
+        var url = './sound/2014Soccer.mp3';
+        sound.load(url);
+        sound.addEventListener(LEvent.COMPLETE,loadOver);
+    }else if(sound.playing){
+        sound.stop();
+    }else{
+        sound.play();
+    }
 }
 
-function loadOver(event){
+function loadOver(e){
 	sound.play();
 	
 }
-//音乐停止还不能实现
-function ondown(event){
-	sound.stop();
-	
-}
-
-
 
 //物体碰撞边界
-
+var BoundTop;
 
 function Bound(){
     LGlobal.box2d =  new LBox2d();
     BoundTop = new LSprite();
     backGroundLayer.addChild(BoundTop);
-
     var shapeArray = [
-        [[0,0],[10,0],[10,600],[0,600]],
-        [[0,0],[900,0],[900,10],[0,10]],
-        [[890,0],[900,0],[900,600],[890,600]],
-        [[0,590],[900,590],[900,600],[0,600]]
+        [[65,40],[70,40],[70,560],[65,560]],
+        [[65,40],[845,40],[845,45],[65,45]],
+        [[845,40],[845,560],[840,560],[840,40]],
+        [[65,560],[70,550],[845,550],[845,560]]
     ];
     BoundTop.addBodyVertices(shapeArray,0,0,0,.5,.4,.5);
+
+    //设置球门
+    ballDoor = new LSprite();
+    backGroundLayer.addChild(ballDoor);
+    ballDoor.graphics.drawRect(0,'#f00',[808,250,35,105],false);
+    ballDoor.graphics.drawRect(0,'#f00',[67,248,35,105],false);
+
 }
+
 
 
 
